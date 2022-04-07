803226422f0e5cf00c33bbf843a3d970f9b165a5
diegohd87 <diego.dias@betrybe.com>
2022-04-07 18:14:36 -0300

    action dbdocs

--- dbdocs.sh
+++ dbdocs.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+set -e
 
+echo "Starting Dbdocs proccess"
+
+sudo npm install -g dbdocs 
+dbdocs
+dbdocs build ./docs/lms-database.dbml --project=$REPOSITORY --password=57KY3ccUTQWZktrQDjvMmcjAShf4BnmU
+
+echo "Success!"
