bfaf8728325bcb2f61ff423c4535bc15006e702c
diegohd87 <diego.dias@betrybe.com>
2022-04-08 14:40:02 -0300

    refactor

--- dbdocs.sh
+++ dbdocs.sh
@@ -3,8 +3,12 @@ set -e
 
 echo "Starting Dbdocs proccess"
 
+DBDOCS_PASSWORD=$(echo -n "$REPOSITORY$(date +%s)" | sha1sum | head -c 40)
+
+echo "DBDOCS_PASSWORD=$DBDOCS_PASSWORD" >> $GITHUB_ENV
+
 sudo npm install -g dbdocs 
 dbdocs
-dbdocs build ./docs/lms-database.dbml --project=$REPOSITORY
+dbdocs build $REPOSITORY_PATH/$DBDOCS_PATH/$REPOSITORY.dbml --project=$REPOSITORY --password=DBDOCS_PASSWORD
 
 echo "Success!"
