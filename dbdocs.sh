91ba46f32a59accd401870799133482b5c019bf2
diegohd87 <diego.dias@betrybe.com>
2022-04-11 17:32:09 -0300

    test

--- dbdocs.sh
+++ dbdocs.sh
@@ -8,7 +8,7 @@ DBDOCS_PASSWORD=$(echo -n "$REPOSITORY$(date +%s)" | sha1sum | head -c 40)
 echo "DBDOCS_PASSWORD=$DBDOCS_PASSWORD" >> $GITHUB_ENV
 
 sudo npm install -g dbdocs 
-dbdocs
+#dbdocs
 ls -a
 #dbdocs build ./docs/lms-database.dbml --project=lego-lms
 #dbdocs build docs/$REPOSITORY-database.dbml --project=$REPOSITORY --password=$DBDOCS_PASSWORD
