7feb7fe2d6b3467977c442bfe8beaa9b7f00e684
diegohd87 <diego.dias@betrybe.com>
2022-04-11 15:07:14 -0300

    teste

--- dbdocs.sh
+++ dbdocs.sh
@@ -9,6 +9,6 @@ echo "DBDOCS_PASSWORD=$DBDOCS_PASSWORD" >> $GITHUB_ENV
 
 sudo npm install -g dbdocs 
 dbdocs
-dbdocs build "$REPOSITORY-dastabase.dbml --project=$REPOSITORY --password=DBDOCS_PASSWORD"
+dbdocs build $REPOSITORY/$REPOSITORY-dastabase.dbml --project=$REPOSITORY --password=DBDOCS_PASSWORD
 
 echo "Success!"
