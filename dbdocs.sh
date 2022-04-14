8efc7f43242f9e8eca2cbdb969a626654d64d8c9
diegohd87 <diego.dias@betrybe.com>
2022-04-14 09:39:25 -0300

    monoreppo test

--- dbdocs.sh
+++ dbdocs.sh
@@ -5,13 +5,19 @@ echo "Dbdocs started"
 #Generate password in order to protect the project's diagram url
 DBDOCS_PASSWORD=$(echo -n "$REPOSITORY$(date +%s)" | sha1sum | head -c 40)
 
+echo "REPOSITORY $REPOSITORY"
+echo "REPOSITORY_PATH $REPOSITORY_PATH"
 echo "Install Dbdocs"
 sudo npm install -g dbdocs 
-echo "Check Dbdocs"
-dbdocs
-echo "Buiold Diagram"
+#echo "Check Dbdocs"
+#dbdocs
+echo "Build Diagram"
 dbdocs build $DBDOCS_PATH/$REPOSITORY-database.dbml --project=$REPOSITORY --password=$DBDOCS_PASSWORD
 
 #echo "url=$url" >> $GITHUB_ENV
 echo "DBDOCS_PASSWORD=$DBDOCS_PASSWORD" >> $GITHUB_ENV
 echo "Dbdocs ran successfully!"
+
+#awk
+#pipe | grep 
+
