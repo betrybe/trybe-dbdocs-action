f81bfb5415ca20f603128985d84401c6f1346bdc
diegohd87 <diego.dias@betrybe.com>
2022-04-13 22:23:43 -0300

    test

--- dbdocs.sh
+++ dbdocs.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-set -ex
-
-echo "Dbdocs started"
+set -e
 
 #Generate password in order to protect the project's diagram url
 DBDOCS_PASSWORD=$(echo -n "$REPOSITORY$(date +%s)" | sha1sum | head -c 40)
 
 sudo npm install -g dbdocs 
 dbdocs
-asdf=$(dbdocs build $DBDOCS_PATH/$REPOSITORY-database.dbml --project=$REPOSITORY --password=$DBDOCS_PASSWORD)
+asdf=$(echo -n dbdocs build $DBDOCS_PATH/$REPOSITORY-database.dbml --project=$REPOSITORY --password=$DBDOCS_PASSWORD)
 
 echo $asdf
 echo "DBDOCS_PASSWORD=$DBDOCS_PASSWORD" >> $GITHUB_ENV
