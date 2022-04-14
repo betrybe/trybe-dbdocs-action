e935f1d8231147b6f1905859643c16be4407b413
diegohd87 <diego.dias@betrybe.com>
2022-04-13 22:16:13 -0300

    teste

--- dbdocs.sh
+++ dbdocs.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 set -ex
 
-echo "Starting Dbdocs proccess"
+echo "Dbdocs started"
 
+#Generate password in order to protect the project's diagram url
 DBDOCS_PASSWORD=$(echo -n "$REPOSITORY$(date +%s)" | sha1sum | head -c 40)
-
-echo "DBDOCS_PASSWORD=$DBDOCS_PASSWORD" >> $GITHUB_ENV
 
 sudo npm install -g dbdocs 
 dbdocs
-#ls -a
-dbdocs build $DBDOCS_PATH/$REPOSITORY-database.dbml --project=$REPOSITORY --password=$DBDOCS_PASSWORD
+asdf=$(dbdocs build $DBDOCS_PATH/$REPOSITORY-database.dbml --project=$REPOSITORY --password=$DBDOCS_PASSWORD)
 
-
-echo '
- _____            _
-/__   \_ __ _   _| |__   ___
-  / /\/ `__| | | | `_ \ / _ \
- / /  | |  | |_| | |_) |  __/
- \/   |_|   \__, |_.__/ \___|
-            |___/
-   ___ _       _         __
-  / _ \ | __ _| |_ __ _ / _| ___  _ __ _ __ ___   __ _
- / /_)/ |/ _` | __/ _` | |_ / _ \| `__| `_ ` _ \ / _` |
-/ ___/| | (_| | || (_| |  _| (_) | |  | | | | | | (_| |
-\/    |_|\__,_|\__\__,_|_|  \___/|_|  |_| |_| |_|\__,_|
-'
-
-echo "Success!"
+echo $asdf
+echo "DBDOCS_PASSWORD=$DBDOCS_PASSWORD" >> $GITHUB_ENV
+echo "Dbdocs ran successfully!"
