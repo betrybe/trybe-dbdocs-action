2bf7e8d3e113432318ed01bc3294e32623952091
diegohd87 <diego.dias@betrybe.com>
2022-04-20 19:08:41 -0300

    asdf

--- dbdocs.sh
+++ dbdocs.sh
@@ -13,13 +13,14 @@ DBDOCS_PASSWORD=$(echo -n "$REPOSITORY$(date +%s)" | sha1sum | head -c 40)
 echo "REPOSITORY $REPOSITORY"
 echo "REPOSITORY_PATH $REPOSITORY_PATH"
 echo "DBDOCS_PATH $DBDOCS_PATH"
+
 echo "Install Dbdocs"
 sudo npm install -g dbdocs 
 #echo "Check Dbdocs"
 #dbdocs
 echo "Build Diagram"
 
-dbdocs build $DBDOCS_PATH/$REPOSITORY-database.dbml --project=$REPOSITORY --password=$DBDOCS_PASSWORD
+dbdocs build docs/$REPOSITORY-database.dbml --project=$REPOSITORY --password=$DBDOCS_PASSWORD
 
 #https://dbdocs.io/username/your_project
 url=https://dbdocs.io/tech-ops/$REPOSITORY
