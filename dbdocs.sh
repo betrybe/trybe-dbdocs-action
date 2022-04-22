a7234801210c92dadfbf3304a559308b60bc8786
diegohd87 <diego.dias@betrybe.com>
2022-04-22 09:20:58 -0300

    asdf

--- dbdocs.sh
+++ dbdocs.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Dbdocs started"
 
-FILENAME = $DBDOCS_FILENAME
+FILENAME=$DBDOCS_FILENAME
 #Generate password in order to protect the project's diagram url
 DBDOCS_PASSWORD=$(echo -n "$REPOSITORY$(date +%s)" | sha1sum | head -c 40)
 
@@ -20,7 +20,7 @@ echo "Build Diagram"
 #dbdocs build docs/$REPOSITORY-database.dbml --project=$REPOSITORY --password=$DBDOCS_PASSWORD
 
 #https://dbdocs.io/username/your_project
-url=https://dbdocs.io/tech-ops/$REPOSITORY
+#url=https://dbdocs.io/tech-ops/$REPOSITORY
 echo "DBDOCS_URL=$url" >> $GITHUB_ENV
 echo "DBDOCS_PASSWORD=$DBDOCS_PASSWORD" >> $GITHUB_ENV
 echo "url=$url" >> $GITHUB_ENV
