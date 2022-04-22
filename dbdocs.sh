d7b6b44c377702540485cc3a88ec347d8fb6f14d
diegohd87 <diego.dias@betrybe.com>
2022-04-22 11:05:40 -0300

    asdf

--- dbdocs.sh
+++ dbdocs.sh
@@ -6,19 +6,21 @@ echo "Dbdocs started"
 #Generate password in order to protect the project's diagram url
 DBDOCS_PASSWORD=$(echo -n "$REPOSITORY$(date +%s)" | sha1sum | head -c 40)
 
-#Filepath 
+#File path through filename
 PATH=$(echo $(find $(pwd -L) -name $DBDOCS_FILENAME))
 
 echo "DBDOCS_FILENAME $DBDOCS_FILENAME"
 echo "FILE PATH $PATH"
 
 echo "Install Dbdocs"
-#sudo npm install -g dbdocs 
+echo "sudo npm install -g dbdocs"
+
 #echo "Check Dbdocs"
 #dbdocs
-echo "Build Diagram"
 
-#dbdocs build docs/$REPOSITORY-database.dbml --project=$REPOSITORY --password=$DBDOCS_PASSWORD
+#Building dbdocs
+echo "Build Diagram"
+echo "dbdocs build $PATH --password=$DBDOCS_PASSWORD"
 
 #https://dbdocs.io/username/your_project
 #url=https://dbdocs.io/tech-ops/$REPOSITORY
