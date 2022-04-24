9264fa7f0ae5afbee8fe109b5fa414e320dcaa39
diegohd87 <diego.dias@betrybe.com>
2022-04-24 17:27:21 -0300

    asdf

--- dbdocs.sh
+++ dbdocs.sh
@@ -12,7 +12,7 @@ REMOVE_PATH="/home/runner/work/$REPOSITORY/$REPOSITORY/$REPOSITORY_PATH"
 URL=$(echo "${PATH/$REMOVE_PATH/""}")
 PROJECT=$REPOSITORY
 echo "Install Dbdocs"
-echo "sudo npm install -g dbdocs"
+echo "$(sudo npm install -g dbdocs)"
 
 #echo "Check Dbdocs"
 #dbdocs
@@ -20,7 +20,7 @@ echo "sudo npm install -g dbdocs"
 #Building dbdocs
 echo "Build Diagram"
 #echo "dbdocs build $url --password=$DBDOCS_PASSWORD"
-echo $(dbdocs build ./$URL --project=$PROJECT)
+echo "$(dbdocs build ./$URL --project=$PROJECT)"
 #https://dbdocs.io/username/your_project
 #echo "https://dbdocs.io/tech-ops/$REPOSITORY"
 echo "DBDOCS_PASSWORD=$DBDOCS_PASSWORD" >> $GITHUB_ENV
