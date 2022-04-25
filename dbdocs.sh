89c433492c56c3ab1274dc65899204ed87466954
diegohd87 <diego.dias@betrybe.com>
2022-04-25 13:36:20 -0300

    asdf

--- dbdocs.sh
+++ dbdocs.sh
@@ -10,7 +10,7 @@ DBDOCS_PASSWORD=$(echo -n "$REPOSITORY$(date +%s)" | sha1sum | head -c 40)
 #PATH=$(echo $(find $(pwd -L) -name $DBDOCS_FILENAME))
 PATH=""
 REMOVE_PATH="/home/runner/work/$REPOSITORY/$REPOSITORY/$REPOSITORY_PATH"
-URL=$(echo "${PATH/$REMOVE_PATH/""}")
+#URL=$(echo "${PATH/$REMOVE_PATH/""}")
 
 #Set value to dbdocs project parameter 
 #PROJECT=$REPOSITORY
